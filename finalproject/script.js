25b92236df989f5858b7fbe300f66816cddbc878
asissenoffa <210103093@stu.sdu.edu.kz>
2022-12-19 16:12:41 +0500

    Update script.js
    asem

--- finalproject/script.js
+++ finalproject/script.js
@@ -46,29 +46,7 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
-var swiper = new Swiper(".product-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-    },
-});
+
 
 var swiper = new Swiper(".review-slider", {
     loop:true, 
@@ -91,7 +69,7 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 
-var swiper = new Swiper(".blogs-slider", {
+var swiper = new Swiper(".guides-slider", {
     loop:true, 
     grabCursor:true,
     spaceBetween: 10,
